0b1d72df1f52d543b5729634e6ce7e70816e5445
Brandon Geren <brandongeren@gmail.com>
2018-02-07 20:41:35 -0600

    Fix early stopping

--- main.py
+++ main.py
@@ -37,91 +37,91 @@ def main(argv):
 
     with tf.name_scope('linear_model') as scope:
 
-        # hidden_1 = tf.layers.dense(x_normalized,
-        #                          420,
-        #                          activation=tf.nn.relu,
-        #                          name='hidden_layer_1')
-
-        # dropout_1 = tf.nn.dropout(hidden_1, keep_prob)
-        # keep_prob = update_keep_prob(keep_prob, keep_prob_updater_rate)
-
-        # hidden_2 = tf.layers.dense(dropout_1,
-        #                          225,
-        #                          activation=tf.nn.relu,
-        #                          name='hidden_layer_2')
-        
-        # dropout_2 = tf.nn.dropout(hidden_2, keep_prob)
-        # keep_prob = update_keep_prob(keep_prob, keep_prob_updater_rate)
-
-        # hidden_3 = tf.layers.dense(dropout_2,
-        #                          121,
-        #                          activation=tf.nn.relu,
-        #                          name='hidden_layer_3')
-
-        # dropout_3 = tf.nn.dropout(hidden_3, keep_prob)
-        # keep_prob = update_keep_prob(keep_prob, keep_prob_updater_rate)
-
-        # hidden_4 = tf.layers.dense(dropout_3,
-        #                          65,
-        #                          activation=tf.nn.relu,
-        #                          name='hidden_layer_4')
-        
-        # dropout_4 = tf.nn.dropout(hidden_4, keep_prob)
-        # keep_prob = update_keep_prob(keep_prob, keep_prob_updater_rate)
-
-        # hidden_5 = tf.layers.dense(dropout_4,
-        #                          35,
-        #                          activation=tf.nn.relu,
-        #                          name='hidden_layer_5')
-
-        # dropout_5 = tf.nn.dropout(hidden_5, keep_prob)
-        # keep_prob = update_keep_prob(keep_prob, keep_prob_updater_rate)
-
-        # hidden_6 = tf.layers.dense(dropout_5,
-        #                          19,
-        #                          activation=tf.nn.relu,
-        #                          name='hidden_layer_6')
-        
-        # dropout_6 = tf.nn.dropout(hidden_6, keep_prob)
-
-
-        # output = tf.layers.dense(dropout_6,
-        #                          10,
-        #                          name='output')
-
         hidden_1 = tf.layers.dense(x_normalized,
                                  420,
                                  activation=tf.nn.relu,
                                  name='hidden_layer_1')
 
-        hidden_2 = tf.layers.dense(hidden_1,
+        dropout_1 = tf.nn.dropout(hidden_1, keep_prob)
+        keep_prob = update_keep_prob(keep_prob, keep_prob_updater_rate)
+
+        hidden_2 = tf.layers.dense(dropout_1,
                                  225,
                                  activation=tf.nn.relu,
                                  name='hidden_layer_2')
+        
+        dropout_2 = tf.nn.dropout(hidden_2, keep_prob)
+        keep_prob = update_keep_prob(keep_prob, keep_prob_updater_rate)
 
-        hidden_3 = tf.layers.dense(hidden_2,
+        hidden_3 = tf.layers.dense(dropout_2,
                                  121,
                                  activation=tf.nn.relu,
                                  name='hidden_layer_3')
 
-        hidden_4 = tf.layers.dense(hidden_3,
+        dropout_3 = tf.nn.dropout(hidden_3, keep_prob)
+        keep_prob = update_keep_prob(keep_prob, keep_prob_updater_rate)
+
+        hidden_4 = tf.layers.dense(dropout_3,
                                  65,
                                  activation=tf.nn.relu,
                                  name='hidden_layer_4')
+        
+        dropout_4 = tf.nn.dropout(hidden_4, keep_prob)
+        keep_prob = update_keep_prob(keep_prob, keep_prob_updater_rate)
 
-        hidden_5 = tf.layers.dense(hidden_4,
+        hidden_5 = tf.layers.dense(dropout_4,
                                  35,
                                  activation=tf.nn.relu,
                                  name='hidden_layer_5')
 
-        hidden_6 = tf.layers.dense(hidden_5,
+        dropout_5 = tf.nn.dropout(hidden_5, keep_prob)
+        keep_prob = update_keep_prob(keep_prob, keep_prob_updater_rate)
+
+        hidden_6 = tf.layers.dense(dropout_5,
                                  19,
                                  activation=tf.nn.relu,
                                  name='hidden_layer_6')
         
-        output = tf.layers.dense(hidden_6,
+        dropout_6 = tf.nn.dropout(hidden_6, keep_prob)
+
+
+        output = tf.layers.dense(dropout_6,
                                  10,
                                  name='output')
+
+        # hidden_1 = tf.layers.dense(x_normalized,
+        #                          420,
+        #                          activation=tf.nn.relu,
+        #                          name='hidden_layer_1')
+
+        # hidden_2 = tf.layers.dense(hidden_1,
+        #                          225,
+        #                          activation=tf.nn.relu,
+        #                          name='hidden_layer_2')
+
+        # hidden_3 = tf.layers.dense(hidden_2,
+        #                          121,
+        #                          activation=tf.nn.relu,
+        #                          name='hidden_layer_3')
+
+        # hidden_4 = tf.layers.dense(hidden_3,
+        #                          65,
+        #                          activation=tf.nn.relu,
+        #                          name='hidden_layer_4')
+
+        # hidden_5 = tf.layers.dense(hidden_4,
+        #                          35,
+        #                          activation=tf.nn.relu,
+        #                          name='hidden_layer_5')
+
+        # hidden_6 = tf.layers.dense(hidden_5,
+        #                          19,
+        #                          activation=tf.nn.relu,
+        #                          name='hidden_layer_6')
+        
+        # output = tf.layers.dense(hidden_6,
+        #                          10,
+        #                          name='output')
 
 
     # define classification loss
@@ -147,7 +147,7 @@ def main(argv):
 
         onehot_encoder = OneHotEncoder(sparse=False)
 
-        best_class_rate = float("inf")
+        best_class_rate = float("-inf")
         best_epoch = -1
         for epoch in range(FLAGS.max_epoch_num):
             print('Epoch: ' + str(epoch))
@@ -184,7 +184,7 @@ def main(argv):
                 
             print('VALIDATION CLASSIFICATION RATE: ' + str(class_rate))
 
-            if (class_rate < best_class_rate):
+            if (class_rate > best_class_rate):
                 print('New best found!')
                 best_train_loss = avg_train_ce
                 best_valid_loss = avg_valid_ce
