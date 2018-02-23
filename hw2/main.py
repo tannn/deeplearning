044ba59e0abab9f722a23a5fec9faeb0f8dd9e33
Brandon Geren <brandongeren@gmail.com>
2018-02-22 21:52:36 -0600

    Add test set classification

--- hw2/main.py
+++ hw2/main.py
@@ -68,7 +68,7 @@ def main(argv):
 
     sum_cross_entropy = tf.reduce_mean(cross_entropy)
 
-    confusion_matrix_op = tf.confusion_matrix(tf.argmax(y, axis=1), tf.argmax(output, axis=1), num_classes=10)
+    confusion_matrix_op = tf.confusion_matrix(tf.argmax(y, axis=1), tf.argmax(output, axis=1), num_classes=7)
 
     optimizer = tf.train.AdamOptimizer(learning_rate=0.001)
     train_op = optimizer.minimize(cross_entropy)
@@ -80,7 +80,6 @@ def main(argv):
         # run training
         batch_size = FLAGS.batch_size
 
-        best_class_rate = float("-inf")
         best_epoch = -1
 
 
@@ -92,8 +91,14 @@ def main(argv):
             train_labels = train_labels_list[fold]
             valid_images = valid_images_list[fold]
             valid_labels = valid_labels_list[fold]
+            test_images = test_images_list[fold]
+            test_labels = test_labels_list[fold]
             train_num_examples = train_images.shape[0]
             valid_num_examples = valid_images.shape[0]
+            test_num_examples = test_images.shape[0]
+	        best_valid_class_rate = float("-inf")
+
+	       	print('Fold' + str(fold))
 
             for epoch in range(FLAGS.max_epoch_num):
                 print('Epoch: ' + str(epoch))
@@ -115,8 +120,8 @@ def main(argv):
                 for i in range(valid_num_examples // batch_size):
                     batch_xs = valid_images[i*batch_size:(i+1)*batch_size, :]
                     batch_ys = valid_labels.reshape(-1,7)[i*batch_size:(i+1)*batch_size, :]    
-                    test_ce, conf_matrix = session.run([sum_cross_entropy, confusion_matrix_op], {x: batch_xs, y: batch_ys})
-                    ce_vals.append(test_ce)
+                    valid_ce, conf_matrix = session.run([sum_cross_entropy, confusion_matrix_op], {x: batch_xs, y: batch_ys})
+                    ce_vals.append(valid_ce)
                     conf_mxs.append(conf_matrix)
                 avg_valid_ce = sum(ce_vals) / len(ce_vals)
                 print('VALIDATION CROSS ENTROPY: ' + str(avg_valid_ce))
@@ -124,27 +129,55 @@ def main(argv):
                 conf_matrix = sum(conf_mxs)
                 print(str(conf_matrix))
                 correct = 0
-                for i in range(10):
+                for i in range(7):
                     correct += conf_matrix[i, i]
-                class_rate = float(correct) / sum(sum(conf_matrix))
+                valid_class_rate = float(correct) / sum(sum(conf_matrix))
                     
-                print('VALIDATION CLASSIFICATION RATE: ' + str(class_rate))
+                print('VALIDATION CLASSIFICATION RATE: ' + str(valid_class_rate))
 
-                if (class_rate > best_class_rate):
+                # report mean test loss
+                ce_vals = []
+                conf_mxs = []
+                for i in range(test_num_examples // batch_size):
+                    batch_xs = test_images[i*batch_size:(i+1)*batch_size, :]
+                    batch_ys = test_labels.reshape(-1,7)[i*batch_size:(i+1)*batch_size, :]    
+                    test_ce, conf_matrix = session.run([sum_cross_entropy, confusion_matrix_op], {x: batch_xs, y: batch_ys})
+                    ce_vals.append(test_ce)
+                    conf_mxs.append(conf_matrix)
+                avg_valid_ce = sum(ce_vals) / len(ce_vals)
+                print('TEST CROSS ENTROPY: ' + str(avg_test_ce))
+                print('TEST CONFUSION MATRIX:')
+                conf_matrix = sum(conf_mxs)
+                print(str(conf_matrix))
+                correct = 0
+                for i in range(7):
+                    correct += conf_matrix[i, i]
+                test_class_rate = float(correct) / sum(sum(conf_matrix))
+                    
+                print('TEST CLASSIFICATION RATE: ' + str(test_class_rate))
+
+
+                if (valid_class_rate > best_valid_class_rate):
                     print('New best found!')
                     best_train_loss = avg_train_ce
                     best_valid_loss = avg_valid_ce
                     best_epoch = epoch                
-                    best_path_prefix = saver.save(session, os.path.join(save_dir, "homework_1-0"))
+                    best_path_prefix = saver.save(session, os.path.join(save_dir, "homework_1-0.fold" + str(fold)))
                     best_conf_mx = conf_matrix
-                    best_class_rate = class_rate
+                    best_valid_class_rate = valid_class_rate
+                    best_test_class_rate = test_class_rate
                     counter = 0
                 else:
                     counter = counter + 1
 
                 if counter >= grace:
                     break
+            	print('--------------------')
 
+            	###### TODO: add a print of all the best results
+            	###### mirror it after what we did last homeowrk
+            print('--------------------')
+            print('--------------------')
 
 
 
