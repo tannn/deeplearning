47a378750c15f4a72a180b7e9f75fbdc66617518
Tanner <hey@tannermarino.com>
2018-02-22 21:31:37 -0600

    split the train data to create a validation set

--- hw2/main.py
+++ hw2/main.py
@@ -44,10 +44,17 @@ def main(argv):
     test_images_4 = np.load(FLAGS.data_dir + 'test_x_4.npy')
     test_labels_4 = np.load(FLAGS.data_dir + 'test_y_4.npy')
 
+    valid_images_1, test_images_1, valid_labels_1, test_labels_1 = split_data(test_images_1, test_labels_1, 0.1)
+    valid_images_2, test_images_2, valid_labels_2, test_labels_2 = split_data(test_images_2, test_labels_2, 0.1)
+    valid_images_3, test_images_3, valid_labels_3, test_labels_3 = split_data(test_images_3, test_labels_3, 0.1)
+    valid_images_4, test_images_4, valid_labels_4, test_labels_4 = split_data(test_images_4, test_labels_4, 0.1)
+
     train_images_list = [train_images_1, train_images_2, train_images_3, train_images_4]
     train_labels_list = [train_labels_1, train_labels_2, train_labels_3, train_labels_4]
-    valid_images_list = [test_images_1, test_images_2, test_images_3, test_images_4]
-    valid_labels_list = [test_labels_1, test_labels_2, test_labels_3, test_labels_4]
+    test_images_list = [test_images_1, test_images_2, test_images_3, test_images_4]
+    test_labels_list = [test_labels_1, test_labels_2, test_labels_3, test_labels_4]
+    valid_images_list = [valid_images_1, valid_images_2, valid_images_3, valid_images_4]
+    valid_labels_list = [valid_labels_1, valid_labels_2, valid_labels_3, valid_labels_4]
 
     x = tf.placeholder(shape=[None, 16641], dtype=tf.float32, name='input_placeholder')
     x_reshaped = tf.reshape(x, [-1, 129, 129, 1])
