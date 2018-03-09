032023c3b7abd36828fe0018c48a125ebf86b076
Tanner <hey@tannermarino.com>
2018-03-08 18:00:34 -0600

    add PSNR and trainer

--- hw3/main.py
+++ hw3/main.py
@@ -20,6 +20,34 @@ def main(argv):
     cifar10_train_data = np.load(FLAGS.data_dir + 'cifar10_train_data.npy')
     cifar10_test_data = np.load(FLAGS.data_dir + 'cifar10_test_data.npy')
 
+    x = tf.placeholder(shape=[None, 16641], dtype=tf.float32, name='input_placeholder')
+
+    output_dense = dense_block(flat_english, language="english")
+    output = tf.identity(output_dense_english, name='output2')
+
+    optimizer, cross_entropy, train_op = optimizer_block("english", output_dense, y, 0.001)
+    sum_cross_entropy = tf.reduce_mean(cross_entropy_english)
+
+    #peak signal to noise ratio
+    mse = tf.reduce_mean(tf.squared_difference(image_target,output))
+    PSNR = tf.constant(255**2,dtype=tf.float32)
+    PSNR = tf.constant(10,dtype=tf.float32)*utils.log10(PSNR) * 20
+
+
+    with tf.Session() as session:
+
+
+        ce_vals = []
+        for i in range(test_num_examples // batch_size):
+            batch_xs = test_images[i*batch_size:(i+1)*batch_size, :]
+            test_ce = session.run([sum_cross_entropy], {x: batch_xs})
+            ce_vals.append(test_ce)
+        avg_test_ce = sum(ce_vals) / len(ce_vals)
+        print('TEST CROSS ENTROPY: ' + str(avg_test_ce))
+
+
+
+
 
 if __name__ == "__main__":
     tf.app.run()
