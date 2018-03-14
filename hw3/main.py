2777602d9cfd6a23985bc3096cd86ae5d988185c
Brandon Geren <brandongeren@gmail.com>
2018-03-13 20:46:38 -0500

    add a maxcompression model

--- hw3/main.py
+++ hw3/main.py
@@ -10,13 +10,13 @@ flags.DEFINE_integer('max_epoch_num', 100, '')
 FLAGS = flags.FLAGS
 
 def main(argv):
-    batch_size = 64
+    batch_size = 32
     
     # load text file
     with open("path.txt", "r") as f: 
         path_string = f.read().split(sep='\n')[0]
 
-    grace = 20
+    grace = 10
     counter = 0
     epoch = 0
 
